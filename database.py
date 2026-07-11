43361cc4c51ea97c9fa3afad42a99365718c621b
adityabhat2005-stack <adityabhat2005@gmail.com>
2026-07-11 19:24:28 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# IMPORTANT: Replace this string with your real connection URL from your Neon dashboard
-DATABASE_URL = "postgresql://neondb_owner:npg_niFcuI4MR8wL@ep-fancy-sun-aqkf0vbs-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+# Secure lookup method: Reads the hidden string from Render's dashboard environment
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
