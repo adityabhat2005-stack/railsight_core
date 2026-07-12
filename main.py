762e80ec3cf0f9467b0f7988a51411554005c89d
adityabhat2005-stack <adityabhat2005@gmail.com>
2026-07-12 20:36:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,3 +104,9 @@ async def get_transit_matrix(db: Session = Depends(get_db)):
             })
             
     return matrix
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    # Render sets an environment variable named PORT automatically
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
